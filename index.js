a4362bbde1cd3ab3b79aad8724eb0b9883db36eb
Wissididom <30803034+Wissididom@users.noreply.github.com>
2023-11-28 21:22:39 +0100

    Remove validation endpoint

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ import {
 	getScopes,
 	getAuthorizationEndpoint,
 	getAccessTokenByAuthTokenEndpoint,
-	validateTwitchToken,
 	getTokens,
 	setTokens
 } from './twitchApi.js';
@@ -20,64 +19,60 @@ const INCLUDE_UNFOLLOWS = process.env.INCLUDE_UNFOLLOWS.toLowerCase() == 'true';
 
 (async () =>{
 	setInterval(async () => { // Run every 5 seconds
-		await validateTwitchToken(process.env.TWITCH_CLIENT_ID, process.env.TWITCH_CLIENT_SECRET, 'http://localhost', process.env.LOCAL_SERVER_PORT, false).then(async (/*value*/) => {
-			let followers = await getChannelFollowers(process.env.TWITCH_CLIENT_ID, process.env.BROADCASTER_ID);
-			if (!fs.existsSync('lastFollowerList.json')) {
-				fs.writeFileSync('lastFollowerList.json', `${JSON.stringify(followers, null, 4)}\n`);
-				return; // Don't need to compare lists if the old list doesn't exist yet
+		let followers = await getChannelFollowers(process.env.TWITCH_CLIENT_ID, process.env.BROADCASTER_ID);
+		if (!fs.existsSync('lastFollowerList.json')) {
+			fs.writeFileSync('lastFollowerList.json', `${JSON.stringify(followers, null, 4)}\n`);
+			return; // Don't need to compare lists if the old list doesn't exist yet
+		}
+		let lastFollowerList = JSON.parse(fs.readFileSync('lastFollowerList.json', {encoding: 'utf8', flag: 'r'}));
+		let followersToSkip = [];
+		if (!Array.isArray(followers.followers))console.log(JSON.stringify(followers));
+		let changedFollowers = false;
+		for (let follower of followers.followers) {
+			if (lastFollowerList.followers.find(item => {
+				return item.user_id == follower.user_id;
+			})) { // Follower in both lists
+				followersToSkip.push(follower.user_id);
+			} else {
+				// Follower only in new list, aka. channel.follow
+				changedFollowers = true;
+				if (!INCLUDE_FOLLOWS) continue;
+				await fetch(`${process.env.DISCORD_WEBHOOK_URL}?wait=true`, {
+					method: 'POST',
+					headers: {
+						"Content-Type": "application/json"
+					},
+					body: JSON.stringify({
+						content: `**User Followed!**\n**Display-Name**: \`\`${follower.user_name}\`\`\n**User-Name**: \`\`${follower.user_login}\`\`\n**User-ID**: \`\`${follower.user_id}\`\``,
+						allowed_mentions: [] // Do not allow any kind of pings
+					})
+				});
 			}
-			let lastFollowerList = JSON.parse(fs.readFileSync('lastFollowerList.json', {encoding: 'utf8', flag: 'r'}));
-			let followersToSkip = [];
-			if (!Array.isArray(followers.followers))console.log(JSON.stringify(followers));
-			let changedFollowers = false;
-			for (let follower of followers.followers) {
-				if (lastFollowerList.followers.find(item => {
-					return item.user_id == follower.user_id;
-				})) { // Follower in both lists
-					followersToSkip.push(follower.user_id);
-				} else {
-					// Follower only in new list, aka. channel.follow
-					changedFollowers = true;
-					if (!INCLUDE_FOLLOWS) continue;
-					await fetch(`${process.env.DISCORD_WEBHOOK_URL}?wait=true`, {
-						method: 'POST',
-						headers: {
-							"Content-Type": "application/json"
-						},
-						body: JSON.stringify({
-							content: `**User Followed!**\n**Display-Name**: \`\`${follower.user_name}\`\`\n**User-Name**: \`\`${follower.user_login}\`\`\n**User-ID**: \`\`${follower.user_id}\`\``,
-							allowed_mentions: [] // Do not allow any kind of pings
-						})
-					});
-				}
+		}
+		for (let follower of lastFollowerList.followers) {
+			if (followersToSkip.includes(follower.user_id)) continue; // Skip followers that are in both lists
+			if (followers.followers.find(item => {
+				return item.user_id == follower.user_id;
+			})) { // Follower in both lists (shouldn't happen because of followersToSkip handling)
+				followersToSkip.push(follower.user_id);
+			} else {
+				// Follower only in old list, aka. channel.unfollow
+				changedFollowers = true;
+				if (!INCLUDE_UNFOLLOWS) continue;
+				await fetch(`${process.env.DISCORD_WEBHOOK_URL}?wait=true`, {
+					method: 'POST',
+					headers: {
+						"Content-Type": "application/json"
+					},
+					body: JSON.stringify({
+						content: `**User Unfollowed!**\n**Display-Name**: \`\`${follower.user_name}\`\`\n**User-Name**: \`\`${follower.user_login}\`\`\n**User-ID**: \`\`${follower.user_id}\`\``,
+						allowed_mentions: [] // Do not allow any kind of pings
+					})
+				});
 			}
-			for (let follower of lastFollowerList.followers) {
-				if (followersToSkip.includes(follower.user_id)) continue; // Skip followers that are in both lists
-				if (followers.followers.find(item => {
-					return item.user_id == follower.user_id;
-				})) { // Follower in both lists (shouldn't happen because of followersToSkip handling)
-					followersToSkip.push(follower.user_id);
-				} else {
-					// Follower only in old list, aka. channel.unfollow
-					changedFollowers = true;
-					if (!INCLUDE_UNFOLLOWS) continue;
-					await fetch(`${process.env.DISCORD_WEBHOOK_URL}?wait=true`, {
-						method: 'POST',
-						headers: {
-							"Content-Type": "application/json"
-						},
-						body: JSON.stringify({
-							content: `**User Unfollowed!**\n**Display-Name**: \`\`${follower.user_name}\`\`\n**User-Name**: \`\`${follower.user_login}\`\`\n**User-ID**: \`\`${follower.user_id}\`\``,
-							allowed_mentions: [] // Do not allow any kind of pings
-						})
-					});
-				}
-			}
-			if (changedFollowers)
-				fs.writeFileSync('lastFollowerList.json', `${JSON.stringify(followers, null, 4)}\n`);
-		}).catch(async (err) => {
-			console.trace(err);
-		});
+		}
+		if (changedFollowers)
+			fs.writeFileSync('lastFollowerList.json', `${JSON.stringify(followers, null, 4)}\n`);
 	}, 5000);
 })();
 
--- twitchApi.js
+++ twitchApi.js
@@ -1,6 +1,4 @@
-import * as fs from 'node:fs';
-
-import open, {openApp, apps} from 'open';
+import * as fs from 'fs';
 
 var tokens = {
 	access_token: 'N/A',
@@ -51,7 +49,7 @@ async function getUser(clientId, accessToken, login) {
 }
 
 // https://dev.twitch.tv/docs/api/reference/#get-channel-followers
-async function getChannelFollowers(clientId, broadcasterId, paginationCursor = null) {
+async function getChannelFollowers(broadcasterId, paginationCursor = null) {
 	let apiUrl;
 	if (paginationCursor) {
 		apiUrl = `https://api.twitch.tv/helix/channels/followers?broadcaster_id=${broadcasterId}&first=100&after=${paginationCursor}`;
@@ -61,12 +59,38 @@ async function getChannelFollowers(clientId, broadcasterId, paginationCursor = n
 	const res = await fetch(apiUrl, {
 		method: 'GET',
 		headers: {
-			'Client-ID': clientId,
+			'Client-ID': process.env.TWITCH_CLIENT_ID,
 			'Authorization': `Bearer ${tokens.access_token}`,
 			'Content-Type': 'application/json'
 		}
 	});
 	const json = await res.json();
+	if (res.status == 401) {
+		console.log(getStatusResponse(res, json));
+		await fetch(getRefreshEndpoint(clientId, clientSecret, tokens.refresh_token), {
+			method: 'POST',
+			headers: {
+				'Client-ID': clientId,
+				'Authorization': `Bearer ${tokens.access_token}`
+			}
+		}).then(res => res.json()).then(res => {
+			if (!res.ok) {
+				console.log('Failed to refresh the token! Try to reauthenticate!');
+				console.log(`Status: ${res.status}; Error-Message: ${res.message}`);
+				console.log(`Open the following Website to authenticate: ${getAuthorizationEndpoint(process.env.TWITCH_CLIENT_ID, process.env.TWITCH_CLIENT_SECRET, 'http://localhost', process.env.LOCAL_SERVER_PORT, getScopes())}`);
+			} else {
+				tokens = res;
+				fs.writeFileSync('.tokens.json', JSON.stringify(res));
+				console.log('Tokens saved!');
+				return 'Tokens successfully refreshed!';
+			}
+		}).catch(err => {
+			console.log('Failed to refresh the token! Try to reauthenticate!');
+			console.error(err);
+			console.log(`Open the following Website to authenticate: ${getAuthorizationEndpoint(process.env.TWITCH_CLIENT_ID, process.env.TWITCH_CLIENT_SECRET, 'http://localhost', process.env.LOCAL_SERVER_PORT, getScopes())}`);
+			throw new Error('Failed to refresh token!');
+		});
+	}
 	if (!res.ok) {
 		throw new Error(getStatusResponse(res, json));
 	}
@@ -100,10 +124,6 @@ function getScopes() {
 	return scopes.join(' ');
 }
 
-function getValidationEndpoint() {
-	return 'https://id.twitch.tv/oauth2/validate';
-}
-
 function getRefreshEndpoint(clientId, clientSecret, refreshToken) {
 	return `https://id.twitch.tv/oauth2/token?grant_type=refresh_token&refresh_token=${encodeURIComponent(refreshToken)}&client_id=${clientId}&client_secret=${clientSecret}`;
 }
@@ -116,65 +136,13 @@ function getAccessTokenByAuthTokenEndpoint(clientId, clientSecret, code, redirec
 	return `https://id.twitch.tv/oauth2/token?client_id=${clientId}&client_secret=${clientSecret}&code=${code}&grant_type=authorization_code&redirect_uri=${encodeURIComponent(redirectUri)}%3A${port}`;
 }
 
-async function validateTwitchToken(clientId, clientSecret, redirectUri, port, openBrowser = true) {
-	await fetch(getValidationEndpoint(), {
-		method: 'GET',
-		headers: {
-			'Authorization': `Bearer ${tokens.access_token}`
-		}
-	}).then(res => res.json()).then(async res => {
-		if (res.status) {
-			if (res.status == 401) {
-				console.log('Trying to refresh with the refresh token');
-				await fetch(getRefreshEndpoint(clientId, clientSecret, tokens.refresh_token), {
-					method: 'POST',
-					headers: {
-						'Client-ID': clientId,
-						'Authorization': `Bearer ${tokens.access_token}`
-					}
-				}).then(res => res.json()).then(res => {
-					if (res.status) {
-						console.log('Failed to refresh the token! Try to reauthenticate!');
-						console.log(`Status: ${res.status}; Error-Message: ${res.message}`);
-						console.log(`Open the following Website to authenticate: ${getAuthorizationEndpoint(clientId, clientSecret, redirectUri, port, getScopes())}`);
-						if (openBrowser)
-							open(getAuthorizationEndpoint(clientId, clientSecret, redirectUri, port, getScopes()));
-					} else {
-						tokens = res;
-						fs.writeFileSync('.tokens.json', JSON.stringify(res));
-						console.log('Tokens saved!');
-						return 'Tokens successfully refreshed!';
-					}
-				}).catch(err => {
-					console.log('Failed to refresh the token! Try to reauthenticate!');
-					console.error(err);
-					console.log(`Open the following Website to authenticate: ${getAuthorizationEndpoint(clientId, clientSecret, redirectUri, port, getScopes())}`);
-					if (openBrowser)
-						open(getAuthorizationEndpoint(clientId, clientSecret, redirectUri, port, getScopes()));
-				});
-			} else {
-				console.log(`Status: ${res.status}`);
-				console.log(`Error-Message: ${res.message}`);
-				throw new Error("Tokens couldn't be refreshed!");
-			}
-		} else {
-			console.log('Validating...');
-			console.log(`Login-Name: ${res.login}; User-ID: ${res.user_id}; Expires in: ${res.expires_in} seconds; Scopes: ${res.scopes.join(', ')}`);
-			tokens.expires_in = res.expires_in;
-			return 'Successfully validated!';
-		}
-	});
-}
-
 export {
 	getTokens,
 	setTokens,
 	getUser,
 	getChannelFollowers,
 	getScopes,
-	getValidationEndpoint,
 	getRefreshEndpoint,
 	getAuthorizationEndpoint,
-	getAccessTokenByAuthTokenEndpoint,
-	validateTwitchToken
+	getAccessTokenByAuthTokenEndpoint
 };
